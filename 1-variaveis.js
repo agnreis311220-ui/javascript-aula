4da9be51d515a4efa8159beb6ca1c1c6c8df483f
agnes dos reis <pedroafpovoa@gmail.com>
2026-05-08 20:02:02 -0300

    javascript

---
+++ 1-variaveis.js
@@ -0,0 +1,25 @@
+//variaveis
+//oque são variáveis? são espaço na memoria para guardar determinado dado
+let nome = "Brendon";
+
+//imprimindo nome
+console.log(nome)
+
+//tipos de variaveis
+let numero = 10;
+let podebeber = true; //booleano
+nome = "Agnes";
+
+console.log(numero)//number
+console.log(podebeber)//bool
+console.log(nome)//string
+
+//concatenção
+
+console.log("olá meu nome é"+nome);
+
+console.log(10+10); //20
+console.log("10"+10); //1010
+console.log("10"-10); //0
+
+
---
+++ 2-operadoreslogicos.js
@@ -0,0 +1,60 @@
+//operadores aritiméticos
+
+//variaveis
+let numero1 =6;
+let numero2 =7;
+
+
+//soma
+
+let soma = numero1 + numero2
+console.log(soma)
+
+//subtração
+let subtração = numero1 - numero2
+console.log(subtração)
+
+//divisão
+let div = numero1/numero2
+console.log(div)
+
+//multiplicação
+let multi = numero1 * numero2
+console.log(multi)
+
+//resto da divisão
+let restoDiv = numero1 % numero2
+console.log(restodiv)
+
+//codigo para descobrir se é par ou impar
+if(numero1%numero2 === 0){
+    console.log("é par")
+}else{
+    console.log("é impar")
+}
+
+//operadores de comparação
+
+let idade = 26;
+
+//maior que
+console.log(idade < 18);
+
+//menor que
+console.log(idade < 18);
+
+//igualdade
+console.log(idade == 18); //compara os valores e não o tipo//false
+
+console.log(idade === 18);//compara o valor e tipo//false
+
+console.log("10" == 10); //true
+console.log("10" === 10); //false
+
+//maior ou igual
+console.log(idade >= 18);//true
+
+//menor ou igual
+console.log(idade <= 18);//false
+
+
